febe4abf2f740b8d00855cdd5235b8458c0a64e7
David Wilemski <david@wilemski.org>
2021-12-07 21:01:53 -0600

    2021 day 7 part 2

--- 2021/day7/src/main.rs
+++ 2021/day7/src/main.rs
@@ -16,6 +16,10 @@ fn main() -> Result<(), std::io::Error> {
 
     println!("min_cost_position: {}, min_cost: {}", min_cost_position, min_cost);
 
+    let (min_cost_position, min_cost) = find_min_cost_additional(&positions);
+
+    println!("with step up: min_cost_position: {}, min_cost: {}", min_cost_position, min_cost);
+
     Ok(())
 }
 
@@ -34,6 +38,31 @@ fn find_min_cost(positions: &Vec<i32>) -> (i32, i32) {
     (min_cost_position, min_cost)
 }
 
+fn find_min_cost_additional(positions: &Vec<i32>) -> (i32, i64) {
+    let max_position = positions.iter().max().expect("expecting max");
+    let mut min_cost = i64::max_value();
+    let mut min_cost_position = i32::max_value();
+    for pos in 0..*max_position {
+        let cost = positions.iter().map(|p| cost_step_up((p - pos).abs() as i64)).sum();
+        if cost < min_cost {
+            min_cost = cost;
+            min_cost_position = pos;
+        }
+    }
+
+    (min_cost_position, min_cost)
+}
+
+fn cost_step_up(v: i64) -> i64 {
+    if v == 0 {
+        0
+    } else if v == 1 {
+        1
+    } else {
+        v + cost_step_up(v - 1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +72,12 @@ mod tests {
         let positions = vec![16,1,2,0,4,2,7,1,2,14];
 
         assert_eq!(find_min_cost(&positions), (2, 37));
+    }
+
+    #[test]
+    fn test_find_min_cost_additional_for_example() {
+        let positions = vec![16,1,2,0,4,2,7,1,2,14];
+
+        assert_eq!(find_min_cost_additional(&positions), (5, 168));
     }
 }
